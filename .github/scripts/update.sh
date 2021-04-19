fad3491528128df4fef81ab79a6c60179b9756a1
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-04-19 13:33:12 +0200

    adding git actions

---
+++ .github/scripts/update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+JOAL_URL="https://api.github.com/repos/anthonyraymond/joal/tags"
+
+LAST_VERSION=$(curl -SsL ${JOAL_URL} | jq .[0].name -r )
+
+sed -i -e "s|JOAL_VERSION='.*'|JOAL_VERSION='${LAST_VERSION}'|" Dockerfile_*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
